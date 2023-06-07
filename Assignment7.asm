066cddc99f4ea3e2884df59398ec1cc1bf768917
shubhamchitampalle <115708526+shubhamchitampalle@users.noreply.github.com>
2023-06-07 23:15:45 +0530

    Assignment7.asm

--- Assignment7.asm
+++ Assignment7.asm
@@ -1,6 +1,6 @@
-;Write X86/64 ALP to perform non-overlapped block transfer without string specific
-;instructions. Block containing data can be defined in the data segment.
-;
+;Non-Overlapped block data transfer
+;Write X86/64 ALP to perform non-overlapped block transfer without string specific instructions. Block containing data can be defined in the data segment.
+
 section .data
 
 %macro write 2
